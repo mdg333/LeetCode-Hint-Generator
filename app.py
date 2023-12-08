12bdf70e468da1eb1dc0ead3f1bbe2dcadebc133
mdg333 <55594281+mdg333@users.noreply.github.com>
2023-12-08 15:44:16 -0800

    Implement backend
    Adds a backend that connects to ChatGPT. Also, adds fetching and full functionality to all dynamic elements.

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, url_for
+from flask_cors import CORS, cross_origin
+from markupsafe import escape
+from openai import OpenAI
+from urllib.parse import unquote
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/login')
+def login():
+    return 'login123'
+
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+
+@app.route('/generate/<problem>/<code>/<difficulty>/')
+def generate_hint(problem, code, difficulty):
+    client = OpenAI(
+        # defaults to os.environ.get("OPENAI_API_KEY")
+        api_key="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",
+    )
+
+    content = ("For the coding problem \"" + unquote(unquote(problem)) + "\" what is a "
+           + difficulty + " hint for someone who has this code \"" + unquote(unquote(code)) + "\"?")
+
+    print()
+    print(content)
+    print()
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant designed to provide coding hints. A small hint is purely"
+                           " conceptual. A medium hint can include some code. A large hint can include a lot of code."},
+            {
+                "role": "user",
+                "content": content,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,18 +3,47 @@ import React, { useState } from 'react'
 
 function App () {
   const [open, setOpen] = useState(false)
+  const [attempt, setAttempt] = useState([]);
+  const [prompt, setPrompt] = useState([]);
+  const [response, setResponse] = useState([]);
+  const [diff, setDiff] = useState(['small']);
+  const [history, setHistory] = useState(['']);
+
+  const handleChangeAttempt = (e) => {
+    setAttempt(e.target.value);
+  };
+
+  const handleChangePrompt = (e) => {
+    setPrompt(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:5000/generate/' + encodeURIComponent(encodeURIComponent(prompt))
+      + "/" + encodeURIComponent(encodeURIComponent(attempt)) + "/" + diff + "/")
+      .then(response => response.text())
+      .then(data => {
+        let success = !data.includes('404 Not Found');
+        console.log(success);
+        if (success) {
+          // setResponse(data.split('content=\'')[1].split('\'')[0]);
+          setResponse(data);
+          setHistory("* " + prompt + "\n" + history)
+        } else {
+          setResponse('Invalid Input!');
+        }
+      });
+  };
+
+  const capitalize = (s) => {
+    return String(s).charAt(0).toUpperCase() + String(s).slice(1);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="History-top">History</div>
-        <div className="History-bottom">* Given an array of integers nums and an integer target, return indices of the
-          two numbers such that they add up to target.
-
-          You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-          You can return the answer in any order.
-        </div>
+        <div className="History-bottom">{history}</div>
         <div className="Title">LeetCode Hint Generator</div>
         <div className="Dropdown-container">
           <div className="Dropdown-trigger" onClick={() => {setOpen(!open)}}>
@@ -22,16 +51,18 @@ function App () {
           </div>
           <div className={'Dropdown-menu ' + (open ? 'active' : 'inactive')}>
             <ul>
-              <DropdownItem text={'Level 1'}/>
-              <DropdownItem text={'Level 2'}/>
-              <DropdownItem text={'Level 3'}/>
+              <div onClick={() => setDiff('small')}><DropdownItem text={'Small'}/></div>
+              <div onClick={() => setDiff('medium')}><DropdownItem text={'Medium'}/></div>
+              <div onClick={() => setDiff('large')}><DropdownItem text={'Large'}/></div>
             </ul>
           </div>
         </div>
-        <button className="Generate">Generate Hint</button>
-        <input className="Response"/>
-        <input className="Attempt" placeholder="Enter current code here."/>
-        <input className="Prompt" placeholder="Enter prompt text here."/>
+        <form onSubmit={handleSubmit}>
+          <textarea className="Response" value={response}/>
+          <input className="Attempt" placeholder="Enter current code here." onChange={handleChangeAttempt}/>
+          <input className="Prompt" placeholder="Enter prompt text here." onChange={handleChangePrompt}/>
+          <button className="Generate">Generate {capitalize(diff)} Hint</button>
+        </form>
       </header>
     </div>
   )
